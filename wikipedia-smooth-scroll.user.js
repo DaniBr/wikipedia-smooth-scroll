3916115d5480443964a7152cefa0421649b9faf5
Daniel Bronshtein <daniel.bronshtein@gmail.com>
2025-03-26 22:25:56 +0200

    @source -> @homepage

--- wikipedia-smooth-scroll.user.js
+++ wikipedia-smooth-scroll.user.js
@@ -3,7 +3,7 @@
 // @description  Adds smooth scrolling for in-page links on Wikipedia and sister sites
 // @version      0.2
 // @author       Daniel Bronshtein
-// @source       https://github.com/DaniBr/wikipedia-smooth-scroll
+// @homepage     https://github.com/DaniBr/wikipedia-smooth-scroll
 // @grant        none
 // @run-at       document-idle
 // @match https://*.wikipedia.org/wiki/*
